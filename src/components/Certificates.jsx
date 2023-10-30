c292adf5bdac716e34960cf4d73003e56307d2a7
Faizan <81250304+Faizanishfaqahmed@users.noreply.github.com>
2023-10-30 14:33:10 +0500

    Initial commit

---
+++ src/components/Certificates.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import {Tilt} from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { github } from "../assets";
+import { SectionWrapper } from "../hoc";
+import { Certificate } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ProjectCard = ({
+  index,
+  name,
+  description,
+  tags,
+  image,
+  source_code_link,
+  logo,
+}) => {
+  return (
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+      <Tilt
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full ml-40'
+      >
+        <div className='relative w-full h-[230px] '>
+          <img
+            src={image}
+            alt='project_image'
+            className='w-full h-full object-cover rounded-2xl'
+          />
+
+          <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
+            <div
+              onClick={() => window.open(source_code_link, "_blank")}
+              className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
+            >
+              <img
+                src={logo}
+                alt='source code'
+                className='w-1/2 h-1/2 object-contain'
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className='mt-5'>
+          <h3 className='text-white font-bold text-[24px]'>{name}</h3>
+          <p className='mt-2 text-secondary text-[14px]'>{description}</p>
+        </div>
+
+        <div className='mt-4 flex flex-wrap gap-2'>
+          {tags.map((tag) => (
+            <p
+              key={`${name}-${tag.name}`}
+              className={`text-[14px] ${tag.color}`}
+            >
+              #{tag.name}
+            </p>
+          ))}
+        </div>
+      </Tilt>
+    </motion.div>
+  );
+};
+
+const Certificates = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+       
+        <h2 className={`${styles.sectionHeadText}`}>Certificates and Achievements</h2>
+      </motion.div>
+
+      <div className='w-full flex'>
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
+        >
+         Welcome to my portfolio, where I take pride in showcasing my journey of continuous learning and professional development. Below, you'll
+          find a curated collection of certificates and achievements that represent my dedication to mastering various skills and technologies.
+        </motion.p>
+      </div>
+
+      <div className='mt-20 flex flex-wrap gap-10 '>
+        {Certificate.map((project, index) => (
+          <ProjectCard key={`project-${index}`} index={index} {...project}className="ml-20"/>
+        ))}
+      </div>
+    </>
+  ); 
+};
+
+export default SectionWrapper(Certificates, "");
---
+++ src/constants/index.js
@@ -0,0 +1,364 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    starbucks,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+    RRC,
+    CFW,
+    JADC,
+    RWD,
+    Freecode,
+    Udemy,
+    fiverr,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Frontend Developer",
+      icon: web,
+    },
+    {
+      title: "ReactJs Developer",
+      icon: mobile,
+    },
+    {
+      title: "NextJs Developer",
+      icon: backend,
+    },
+    {
+      title: "Content Creator",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Redux Toolkit",
+      icon: redux,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "figma",
+      icon: figma,
+    },
+    {
+      name: "docker",
+      icon: docker,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "React.js Developer",
+      company_name: "Fiverr",
+      icon: fiverr,
+      iconBg: "#383E56",
+      date: "March 2020 - April 2021",
+      points: [
+        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+    {
+      title: "NextJs Developer",
+      company_name: "SmartSol Technologies",
+      icon: fiverr,
+      iconBg: "#E6DEDD",
+      date: "Jan 2021 - Feb 2022",
+      points: [
+        "Developing and maintaining web applications using Next.js and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+    {
+      title: "Web Developer",
+      company_name: "Shopify",
+      icon: shopify,
+      iconBg: "#383E56",
+      date: "Jan 2022 - Jan 2023",
+      points: [
+        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+    {
+      title: "Full stack Developer",
+      company_name: "Meta",
+      icon: meta,
+      iconBg: "#E6DEDD",
+      date: "Jan 2023 - Present",
+      points: [
+        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make a website as beautiful as our product, but Faizan proved me wrong.",
+      name: "Fazal e Mabood",
+      designation: "CFO",
+      company: "SmartSol Technolgies",
+      image: "",
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their clients' success like Faizan does.",
+      name: "Chris Brown",
+      designation: "COO",
+      company: "DEF Corp",
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "After Faizan optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Lisa Wang",
+      designation: "CTO",
+      company: "456 Enterprises",
+      image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Registration Page",
+      description:
+        "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+      tags: [
+        {
+          name: "HTML",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "CSS",
+          color: "green-text-gradient",
+        },
+        {
+          name: "JavaScript",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: carrent,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Job IT",
+      description:
+        "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "restapi",
+          color: "green-text-gradient",
+        },
+        {
+          name: "scss",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: jobit,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Trip Guide",
+      description:
+        "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+      tags: [
+        {
+          name: "nextjs",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "supabase",
+          color: "green-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: tripguide,
+      source_code_link: "https://github.com/",
+    },
+  ];
+  const Certificate = [
+    {
+      name: "JS, Algo, Data ",
+      description:
+        "JavaScript Algorithms and Data Structures Developers Certification representing 300 hourse of Work",
+      tags: [
+        {
+          name: "JavaScript",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "Algorithms",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Data Structures",
+          color: "pink-text-gradient",
+        },
+      ],
+      logo:Freecode,
+      image: JADC,
+      source_code_link: "https://www.freecodecamp.org/certification/fcce2771b7b-aca8-400a-9b35-1feb346d4f88/javascript-algorithms-and-data-structures",
+    },
+    {
+      name: "Responsive Web Design",
+      description:
+        "Responsive Web Design Developer Certificattion representing 300 hourse of Work.",
+      tags: [
+        {
+          name: "HTML",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "CSS",
+          color: "green-text-gradient",
+        },
+        {
+          name: "JavaScript",
+          color: "pink-text-gradient",
+        },
+      ],
+      logo: Freecode,
+      image: RWD,
+      source_code_link: "https://www.freecodecamp.org/certification/fcce2771b7b-aca8-400a-9b35-1feb346d4f88/responsive-web-design",
+    },
+    {
+      name: "Frontend Development",
+      description:
+        "This certificate above verifies that Faizan successfully completed the course The Complete Front-End Web Development Course on 07/15/2023 as taught by Joseph Delgadillo, Nick Germaine on Udemy.",
+      tags: [
+        {
+          name: "JQuery",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "BootStrape",
+          color: "green-text-gradient",
+        },
+        {
+          name: "JavaScript",
+          color: "pink-text-gradient",
+        },
+      ],
+      logo:Udemy,
+      image: CFW,
+      source_code_link: "https://www.udemy.com/certificate/UC-89ddecbe-9571-4e9f-9e6f-4e839dce727b/",
+    },
+    {
+      name: "ReactJs Redux NextJs",
+      description:
+        "This certificate above verifies that Faizan successfully completed the course React - The Complete Guide 2023 (incl. React Router & Redux) on 09/25/2023 as taught by Academind by Maximilian Schwarzmüller, Maximilian Schwarzmüller on Udemy.",
+      tags: [
+        {
+          name: "ReactJS",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "Redux",
+          color: "green-text-gradient",
+        },
+        {
+          name: "NextJs",
+          color: "pink-text-gradient",
+        },
+      ],
+      logo:Udemy,
+      image: RRC,
+      source_code_link: "https://www.udemy.com/certificate/UC-49e945cd-5a47-4b61-9c93-ad902b463633/",
+    },
+  ];
+  
+  
+  export { services, technologies, experiences, testimonials, projects, Certificate };
